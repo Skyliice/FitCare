c75d75097c27e83e68b9513d417b31d499fe144a
C-INFORM\metelitsa <b.metelitsa@center-inform.ru>
2023-04-26 17:56:18 +0300

    Initial changes

---
+++ FitCare/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+using System.Reactive;
+using System.Reactive.Linq;
+using ReactiveUI;
+
+namespace FitCare.ViewModels;
+
+public class MainViewModel : ReactiveObject
+{
+    private int _counter;
+    public int Counter
+    {
+        get => _counter;
+        set => this.RaiseAndSetIfChanged(ref _counter, value);
+    }
+    public ReactiveCommand<Unit,Unit> ClickCommand;
+
+    public MainViewModel()
+    {
+        ClickCommand = ReactiveCommand.CreateFromObservable(Click);
+
+    }
+
+    private IObservable<Unit> Click()
+    {
+        Counter++;
+        return Observable.Return(Unit.Default);
+    }
+}
---
+++ FitCare/Views/MainView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Reactive.Disposables;
+using FitCare.ViewModels;
+using ReactiveUI;
+using ReactiveUI.Maui;
+
+namespace FitCare;
+
+public partial class MainPage : ReactiveContentPage<MainViewModel>
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        ViewModel = new MainViewModel();
+        this.WhenActivated(disposable =>
+        {
+           
+        });
+    }
+    
+}
